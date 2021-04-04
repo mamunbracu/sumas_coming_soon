a0e52787a4f702deb366f7ea0fc783f0d5fbe533
mamunbracu <niloy.fall16@gmail.com>
2021-04-04 15:13:06 +0600

    finalt commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ComingSoon from './components/ComingSoon/ComingSoon';
+
+function App() {
+  return (
+    <div className="">
+      <ComingSoon />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ComingSoon/ComingSoon.js
@@ -0,0 +1,44 @@
+import React, { Fragment } from "react";
+import "./ComingSoon.css"
+const ComingSoon = () => {
+  return (
+    <Fragment>
+      {/* change the bootstrap container to your build in container */}
+      <div className="coming-wrapper">
+      <div className="container">
+          <div className="row align-items-center">
+           <div className="col-md-3 col-sm-none"></div>
+           <div className="col-md-6 col-sm-12">
+           <div className="align-items-center">
+                  <img src="/assets/sumas_coming_soon_logo.png" alt="SUMASH TECK" className="coming-soon-logo" />
+                  <div className="working mt-4">
+                        <div className="working-with">
+                          <h3>We are working </h3>
+                          <h2>with our website</h2>
+                          <span className="thanks my-5">Thanks for your patience </span>
+                        </div>
+                        <div className="connect-with-us">
+                            <input type="text" placeholder="connect with us"/>
+                            <button>
+                              <img src="/assets/mail.svg" alt="" className="mr-2 email-logo mt-0"/>Mail to us</button>
+                        </div>
+                        <div className="social-links">
+                            <span>Follow Us On :</span>
+                            <a href="/" target="_blank"><img src="/assets/linkedIn.png" alt=""/></a>
+                            <a href="/" target="_blank"><img src="/assets/twitter.png" alt=""/></a>
+                            <a href="/" target="_blank"><img src="/assets/fb.png" alt=""/></a>
+                            <a href="/" target="_blank"><img src="/assets/insta.png" alt=""/></a>
+                            
+                        </div>
+                  </div>
+              </div>
+           </div>
+           <div className="col-md-3 col-sm-none"></div>
+          </div>
+      </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default ComingSoon;
